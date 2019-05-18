72d55d131b1caf2e2ddb8f77bd990ca7af087949
Cloud <wfloatingcloud@outlook.com>
2019-05-18 19:06:08 +0800

    Change Kotlin reflect to Java reflect.

--- README.md
+++ README.md
@@ -21,7 +21,21 @@ data class Film(
 ## 导出到表格
 
 ```kotlin
-val films: List<Film> = ...
-val workbook = ListToExcel(list).toExcel()
+val films = listOf(
+    Film(1, "Iron Man", Date.valueOf("2008-4-30"), "Iron Man is a 2008 American superhero film."),
+    Film(2, "Iron Man 2", Date.valueOf("2010-5-7"), "Iron Man 2 is a 2010 American superhero film."),
+    Film(3, "Iron Man 3", Date.valueOf("2013-5-1"), "Iron Man 3 is a 2013 American superhero film")
+)
+    
+val workbook = ListToExcel(films).toExcel()
 workbook.write(File("/home/films.xls"))
 ```
+
+### 导出结果
+
+ID	|标题	    |上映时间	|简介
+----|-----------|-----------|---------
+1	|Iron Man	|2008-04-30	|Iron Man is a 2008 American superhero film.
+2	|Iron Man 2	|2010-05-07	|Iron Man 2 is a 2010 American superhero film.
+3	|Iron Man 3	|2013-05-01	|Iron Man 3 is a 2013 American superhero film
+
--- src/test/kotlin/cloud/list2excel/util/ListToExcelTest.kt
+++ src/test/kotlin/cloud/list2excel/util/ListToExcelTest.kt
@@ -1,21 +1,15 @@
 package cloud.list2excel.util
 
-import org.junit.Before
 import java.io.File
 import java.sql.Date
 
 class ListToExcelTest {
 
-    private val films: MutableList<Film> = ArrayList()
-
-    @Before
-    fun generateData() {
-        films.run {
-            add(Film(1, "Iron Man", Date.valueOf("2008-4-30"), "None"))
-            add(Film(2, "Iron Man 2", Date.valueOf("2010-5-7"), "None"))
-            add(Film(3, "Iron Man 3", Date.valueOf("2013-5-1"), "None"))
-        }
-    }
+    private val films = listOf(
+        Film(1, "Iron Man", Date.valueOf("2008-4-30"), "Iron Man is a 2008 American superhero film."),
+        Film(2, "Iron Man 2", Date.valueOf("2010-5-7"), "Iron Man 2 is a 2010 American superhero film."),
+        Film(3, "Iron Man 3", Date.valueOf("2013-5-1"), "Iron Man 3 is a 2013 American superhero film")
+    )
 
     @org.junit.Test
     fun toExcel() {
